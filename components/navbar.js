14ea00e6511b48de5c0527e86aa25d636f7b0914
Alif Wibowo <alif.likes.coding@gmail.com>
2024-07-26 03:29:47 +0700

    Half finish NavBar

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,69 @@
-import React from 'react'
-import Container from './container'
+import React from "react";
+import Container from "./container";
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <Container>
-      <div>Navbar</div>
-    </Container>
-  )
-}
+    <section className="font-poppins px-5">
+      <Container>
+        <div className="flex justify-between">
+          <div className="flex pt-[21px] pb-[23px]">
+            <img
+              src="/rembang-logo.png"
+              alt="logo kabupaten rembang"
+              className="w-[61px]"
+            />
+            <img
+              src="/kkn-logo.png"
+              alt="logo kkn"
+              className="w-[61px] ml-[30px] py-[10px]"
+            />
+          </div>
+          <div className="hidden lg:block">
+            <Link
+              href="/"
+              className="mr-[25px] pt-[51px] pb-[50px] inline-block relative group  transition-colors duration-300 ease-in-out hover:text-primary"
+            >
+              Beranda
+              <span className="absolute bottom-10 left-0 w-full h-0.5 bg-primary transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100"></span>
+            </Link>
+            <Link
+              href="/tentang"
+              className="mr-[25px] pt-[51px] pb-[50px] inline-block relative group  transition-colors duration-300 ease-in-out hover:text-primary"
+            >
+              Tentang
+              <span className="absolute bottom-10 left-0 w-full h-0.5 bg-primary transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100"></span>
+            </Link>
+            <Link
+              href="/kumpulan-video"
+              className="mr-[25px] pt-[51px] pb-[50px] inline-block relative group  transition-colors duration-300 ease-in-out hover:text-primary"
+            >
+              Kumpulan Video
+              <span className="absolute bottom-10 left-0 w-full h-0.5 bg-primary transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100"></span>
+            </Link>
+            <Link
+              href="/blog"
+              className="mr-[25px] pt-[51px] pb-[50px] inline-block relative group  transition-colors duration-300 ease-in-out hover:text-primary"
+            >
+              Blog
+              <span className="absolute bottom-10 left-0 w-full h-0.5 bg-primary transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100"></span>
+            </Link>
+            <Link
+              href="/temukan-kami"
+              className="mr-[25px] pt-[51px] pb-[50px] inline-block relative group  transition-colors duration-300 ease-in-out hover:text-primary"
+            >
+              Temukan Kami
+              <span className="absolute bottom-10 left-0 w-full h-0.5 bg-primary transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100"></span>
+            </Link>
+          </div>
+          <div className="block lg:hidden">
+            
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
 
-export default Navbar
+export default Navbar;
